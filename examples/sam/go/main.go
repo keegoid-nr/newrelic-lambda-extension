d89c0b9c3bb3adcfbad46dbed7daf5e52c20a1d9
Keegan Mullaney <kmullaney@newrelic.com>
2022-08-01 14:51:09 -0700

    go work

--- examples/sam/go/main.go
+++ examples/sam/go/main.go
@@ -9,30 +9,30 @@ import (
 )
 
 func handler(ctx context.Context) (string, error) {
-	// At this point, we're handling an invocation. Cold start is over; this code runs for each invocation.
-	// We'd like to add a custom event, and a custom attribute. For that, we need the transaction.
-	if txn := newrelic.FromContext(ctx); nil != txn {
-		// This attribute gets added to the normal AwsLambdaInvocation event
-		txn.AddAttribute("userlevel", "gold")
-		// This is an example of a custom event. `FROM MyGoEvent SELECT *` in New Relic will find this event.
-		txn.Application().RecordCustomEvent("MyGoEvent", map[string]interface{}{
-			"zip": "zap",
-		})
-	}
+  // At this point, we're handling an invocation. Cold start is over; this code runs for each invocation.
+  // We'd like to add a custom event, and a custom attribute. For that, we need the transaction.
+  if txn := newrelic.FromContext(ctx); nil != txn {
+    // This attribute gets added to the normal AwsLambdaInvocation event
+    txn.AddAttribute("userlevel", "gold")
+    // This is an example of a custom event. `FROM MyGoEvent SELECT *` in New Relic will find this event.
+    txn.Application().RecordCustomEvent("MyGoEvent", map[string]interface{}{
+      "zip": "zap",
+    })
+  }
 
-	// As normal, anything you write to stdout ends up in CloudWatch
-	fmt.Println("hello world!")
+  // As normal, anything you write to stdout ends up in CloudWatch
+  fmt.Println("hello world!")
 
-	return "Success!", nil
+  return "Success!", nil
 }
 
 func main() {
-	// Here we are in cold start. Anything you do in main happens once.
-	// In main, we initialize the agent.
-	app, err := newrelic.NewApplication(nrlambda.ConfigOption())
-	if nil != err {
-		fmt.Println("error creating app (invalid config):", err)
-	}
-	// Then we start the lambda handler using `nrlambda` rather than `lambda`
-	nrlambda.Start(handler, app)
+  // Here we are in cold start. Anything you do in main happens once.
+  // In main, we initialize the agent.
+  app, err := newrelic.NewApplication(nrlambda.ConfigOption())
+  if nil != err {
+    fmt.Println("error creating app (invalid config):", err)
+  }
+  // Then we start the lambda handler using `nrlambda` rather than `lambda`
+  nrlambda.Start(handler, app)
 }
